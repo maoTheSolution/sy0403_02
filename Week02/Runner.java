fc90247654a61ac2f6465ee6699e021a32f62327
Minhyuk Adam Oh <maothesolution@gmail.com>
2024-01-23 16:26:20 +0900

    updated

--- Week02/Runner.java
+++ Week02/Runner.java
@@ -1,6 +1,6 @@
 class Runner{
     public static void main(String [] args){
-        Sample01 s01 = new Sample01();
+        Sample01 s01 = new Sample01("Seoyoung");
         String name = "Adam";
         // new Sample01();
     }
--- Week02/Sample01.java
+++ Week02/Sample01.java
@@ -2,4 +2,8 @@ public class Sample01{
     Sample01(){
         System.out.println("This is the original constructor");
     }
+
+    Sample01(String name){
+        System.out.println(name + ", Hello");
+    }
 }
