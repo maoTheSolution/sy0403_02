2e8bf2faf41ba289fc85a5ef4ad8f4257af2e2fa
Minhyuk Adam Oh <maothesolution@gmail.com>
2024-01-30 15:01:40 +0900

    updated

---
+++ Week03/Sample01.java
@@ -0,0 +1,31 @@
+class Student{
+    String name;
+    int age;
+    double height;
+
+    Student(String name, int age, double height){
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    //setter
+
+    //getter
+    public String getName(){
+        return name;
+    }
+
+    public int getAge(){
+        return age;
+    }
+
+    public double getHeight(){
+        return height;
+    }
+}
+
+
+class Sample01{
+    
+}
