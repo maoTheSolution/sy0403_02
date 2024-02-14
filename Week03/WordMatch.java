467cd14a37feade6606e2bf91fb409affb3fd22c
Minhyuk Adam Oh <minhyukoh@Minhyuks-MacBook-Pro.local>
2024-02-14 10:20:14 +0900

    updated

--- Week03/WordMatch.java
+++ Week03/WordMatch.java
@@ -18,9 +18,50 @@ public class WordMatch{
         return score * guess.length() * guess.length();
     }
 
+    public String findBetterGuess(String first, String second){
+        int output = ScoreGuess(first);
+        String result = "";
+        if(output < ScoreGuess(second)){
+            result = second;
+        } else if(output > ScoreGuess(second)){
+            result = first;
+        } 
+        else{
+            result = helper(first, second);
+        }
+
+        return result;
+    }
+
+    public String helper(String first, String second){
+        String result = "";
+        if (first.length() > second.length()){
+            result = first;
+        } else if (first.length() < second.length()){
+            result = second;
+        } else{
+            for(int i=0; i<first.length(); i++){
+                int f = (int)first.charAt(i);
+                int s = (int)second.charAt(i);
+                if(f == s){
+                    continue;
+                } else if(f > s){
+                    result = first;
+                    break;
+                } else{
+                    result = second;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
     public static void main(String [] args){
         WordMatch wm = new WordMatch("libertasas");
-        // System.out.println(wm.given);
+        System.out.println(wm.given);
         System.out.println(wm.ScoreGuess("as"));
+        System.out.println(wm.findBetterGuess("ten", "nation"));
+        System.out.println(wm.findBetterGuess("con", "concat"));
     }
 }
